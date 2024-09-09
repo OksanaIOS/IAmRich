cf4f02d7fbb8e48b516c77d5ea530ac0f60d5fc1
Oksana ogay <example@email.com>
2024-09-09 15:17:47 +0500

    refactoring

--- IamRich/ViewController.swift
+++ IamRich/ViewController.swift
@@ -12,9 +12,19 @@ class ViewController: UIViewController {
     let myLabel = UILabel()
     let myImage = UIImageView()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-//      настраиваем метку и добавляем ее на экран
+    private func createImageView() {
+        //    настраиваем изображение и добавляем его на экран
+        
+        let imageFrame = CGRect(x: 66, y: 313, width: 270, height: 270)
+        myImage.frame = imageFrame
+        self.myImage.image = UIImage(named: "diamond.png")
+        // Сохраняет пропорции, вписывая изображение в frame
+        myImage.contentMode = .scaleAspectFit
+        self.view.addSubview(myImage)
+    }
+    
+    private func createLabel() {
+        //      настраиваем метку и добавляем ее на экран
         let labelFrame = CGRect(x: 113, y: 156, width: 173, height: 48)
         myLabel.frame = labelFrame
         myLabel.text = "I Am Rich"
@@ -27,18 +37,14 @@ class ViewController: UIViewController {
         myLabel.adjustsFontSizeToFitWidth = true
         myLabel.sizeToFit()
         myLabel.textColor = .white
-//        label.center = view.center
         self.view.addSubview(myLabel)
+    }
     
-
-//    настраиваем изображение и добавляем его на экран
-    
-    let imageFrame = CGRect(x: 66, y: 313, width: 270, height: 270)
-        myImage.frame = imageFrame
-        self.myImage.image = UIImage(named: "diamond.png")
-        // Сохраняет пропорции, вписывая изображение в frame
-        myImage.contentMode = .scaleAspectFit
-        self.view.addSubview(myImage)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        вызов функций
+        createLabel()
+        createImageView()
     
             
         }
